085e0c0478929133b6ff8586ab2040fa25581c42
Blake Burbach <cras.bburbach@gmail.com>
2018-03-26 12:00:43 -0500

    Couldn't complete before lunch

--- server/server.js
+++ server/server.js
@@ -3,6 +3,18 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = 5000;
 
+const mongoose = require('mongoose');
+const databaseURL = 'mongodb://localhost:27017/hadar'
+
+mongoose.connect(databaseURL);
+mongoose.connection.on('connected', ()=>{
+    console.log('This pleases the mongod');
+});
+
+mongoose.connection.on('error', ()=>{
+    console.log('The mongod is not pleased');
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
